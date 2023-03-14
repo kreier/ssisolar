5ada45fcdbfd27bac9915ed566015c105530a917
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-03-15 06:08:29 +0700

    documentation of 2022

---
+++ circuitpython/lib/secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk commiting that info or sharing it
+
+secrets = {
+    'ssid'         : 'IOT',
+    'password'     : 'password',
+    'aio_username' : 'kreier',
+    'aio_password' : 'aio_IVVt56isifAEc6sJumw0NUxFCjRz',
+    'timezome'     : "Asia/Ho_Chi?Minh", # http://worldtimeapi.org/timezones
+}
